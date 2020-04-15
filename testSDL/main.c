c60b129ba80f4d9f36543f5d9849e471707666dc
abdlynng <abdlynng@gmail.com>
2020-04-15 14:09:13 +0200

    ajout d'une surface rectangle coloré au lieu de surface ecran

--- testSDL/main.c
+++ testSDL/main.c
@@ -7,6 +7,8 @@ void pause();
 int main(){
 
     SDL_Surface *ecran = NULL;
+    SDL_Surface *rectangle = NULL;
+    SDL_Rect position;
 
     if (SDL_Init(SDL_INIT_VIDEO) == 0){
         // ( largeur, hauteur, nombre de pixel:32 est la valeur max, option:HWSURFACE chargement par le memoire video)
@@ -14,6 +16,15 @@ int main(){
         SDL_WM_SetCaption("Ma premiere Fenetre C",NULL);
         // on colorie toute la fenetre en vert clair
         SDL_FillRect(ecran,NULL,SDL_MapRGB(ecran->format,100,250,100));
+
+        rectangle = SDL_CreateRGBSurface(SDL_HWSURFACE,250,250,32,0,0,0,0);
+        SDL_FillRect(rectangle,NULL,SDL_MapRGB(rectangle->format,100,0,100));
+
+        position.x = 125;
+        position.y = 125;
+        // collage de la surface rectangle dans la surface ecran avec les coordonnees position
+        SDL_BlitSurface(rectangle,NULL,ecran,&position);
+
         SDL_Flip(ecran); // on met a jour le fenetre
         pause();
 
