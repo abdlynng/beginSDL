7f905e9c29ee42a29a1f0d046114cf370bb0e39e
abdlynng <abdlynng@gmail.com>
2020-04-15 15:13:20 +0200

    degradé double on a doublé la hauteur de la fenetre

--- ecran_couleur_degrade/main.c
+++ ecran_couleur_degrade/main.c
@@ -10,14 +10,24 @@ int main(){
     SDL_Surface *ligne[256] = {NULL};
     int i = 0;
     SDL_Rect position;
+    int hauteur = 512;
 
     SDL_Init(SDL_INIT_VIDEO);
-    ecran = SDL_SetVideoMode(600,255,32,SDL_HWSURFACE);
+    ecran = SDL_SetVideoMode(600,hauteur,32,SDL_HWSURFACE);
     SDL_WM_SetCaption("fenetre degrade", NULL);
+
+    for (i=0; i<256; i++){
+        position.x = 0;
+        position.y = i;
+
+        *(ligne+i)= SDL_CreateRGBSurface(SDL_SWSURFACE,600,1,32,0,0,0,0);
+        SDL_FillRect(ligne[i],NULL,SDL_MapRGB(ecran->format,i,i,i));
+        SDL_BlitSurface(ligne[i],NULL,ecran,&position);
+    }
 
     for (i=255; i>=0; i--){
         position.x = 0;
-        position.y = 256-i;
+        position.y = hauteur-i;
 
         *(ligne+i)= SDL_CreateRGBSurface(SDL_SWSURFACE,600,1,32,0,0,0,0);
         SDL_FillRect(ligne[i],NULL,SDL_MapRGB(ecran->format,i,i,i));
@@ -28,7 +38,7 @@ int main(){
 
     pause();
 
-    for (i = 0 ; i <= 255 ; i++) //libérer les 256 surfaces
+    for (i = 0 ; i < hauteur ; i++) //libérer les 256 surfaces
         SDL_FreeSurface(ligne[i]);
 
     SDL_Quit();
