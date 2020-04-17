2f32e9f1559a6879b6d2b9ce87a295752f5b3018
abdlynng <abdlynng@gmail.com>
2020-04-17 14:09:02 +0200

    chargement image de fond format bmp

---
+++ charger_image/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL.h>
+
+void pause();
+
+int main(){
+
+    SDL_Surface *ecran = NULL;
+    SDL_Surface *imageFond = NULL;
+    SDL_Rect positionImageFond;
+
+    positionImageFond.x = 0;
+    positionImageFond.y = 0;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    ecran = SDL_SetVideoMode(900,600,32,SDL_HWSURFACE);
+    SDL_WM_SetCaption("chargement image de fond", NULL);
+
+    imageFond = SDL_LoadBMP("lac_en_montagne.bmp");
+    SDL_BlitSurface(imageFond,NULL,ecran,&positionImageFond);
+
+    SDL_Flip(ecran);
+    pause();
+    SDL_Quit();
+    return EXIT_SUCCESS;
+}
+
+/* pour mettre en pause notre programme
+*  et fquitter la fenetre par le clic du bouton quitter
+*/
+void pause()
+{
+    int continuer = 1;
+    SDL_Event event;
+
+    while (continuer)
+    {
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                continuer = 0;
+        }
+    }
+}
